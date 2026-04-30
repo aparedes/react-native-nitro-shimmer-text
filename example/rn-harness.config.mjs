0d4103ff93e578a27b53f254c00d171fa65a3685
Alejandro Paredes Alva <aparedes@aparedes.net>
2026-04-30 20:02:37 +0200

    Update rn-harness.config.mjs

--- example/rn-harness.config.mjs
+++ example/rn-harness.config.mjs
@@ -1,27 +1,27 @@
 import {
   androidEmulator,
   androidPlatform,
-} from "@react-native-harness/platform-android";
+} from '@react-native-harness/platform-android';
 import {
   applePlatform,
   appleSimulator,
-} from "@react-native-harness/platform-apple";
+} from '@react-native-harness/platform-apple';
 
 export default {
-  entryPoint: "./index.js",
-  appRegistryComponentName: "NitroShimmerTextExample",
+  entryPoint: './index.js',
+  appRegistryComponentName: 'NitroShimmerTextExample',
 
   runners: [
     androidPlatform({
-      name: "medium_phone_api_36.1",
-      device: androidEmulator("Medium_Phone_API_36.1"),
-      bundleId: "com.nitroshimmertextexample",
+      name: 'medium_phone_api_36.1',
+      device: androidEmulator('Medium_Phone_API_36.1'),
+      bundleId: 'com.nitroshimmertextexample',
     }),
     applePlatform({
-      name: "iphone-17",
-      device: appleSimulator("iPhone 17", "26.3"),
-      bundleId: "com.nitroshimmertextexample",
+      name: 'iphone-17',
+      device: appleSimulator('iPhone 17', '26.4'),
+      bundleId: 'com.nitroshimmertextexample',
     }),
   ],
-  defaultRunner: "iphone-17",
+  defaultRunner: 'iphone-17',
 };
